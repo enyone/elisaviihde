93f3abc83b40961b590cc04bf80f7660d043a89e
enyone <enyone@rainio.org>
2015-03-06 17:51:44 +0200

    Changed version to 1.1

--- elisaviihde.py
+++ elisaviihde.py
@@ -1,7 +1,7 @@
 # Elisa Viihde API Python implementation
 # License: GPLv3
 # Author: Juho Tykkala
-# Version: 1.1b
+# Version: 1.1
 # https://github.com/enyone/elisaviihde
 
 import requests, json, re, time, datetime, math
