d971f070f1b1b48a765bf92f8428fd5aa375c8e5
enyone <enyone@rainio.org>
2015-03-08 14:28:16 +0200

    Initial commit of KODI module build script

---
+++ kodi.build.sh
@@ -0,0 +1,4 @@
+rm script.module.elisaviihde.zip
+cp elisaviihde.py script.module.elisaviihde/lib/
+zip -r script.module.elisaviihde.zip script.module.elisaviihde
+rm script.module.elisaviihde/lib/elisaviihde.py
