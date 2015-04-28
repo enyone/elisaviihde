4733186aef928e0e9fbb3cb60e6aff403aebf813
Juho Tykkälä <enyone@phnet.fi>
2015-04-28 22:38:15 +0300

    Version 1.4

--- elisaviihde.py
+++ elisaviihde.py
@@ -1,7 +1,7 @@
 # Elisa Viihde API Python implementation
 # License: GPLv3
 # Author: enyone
-# Version: 1.3
+# Version: 1.4
 # https://github.com/enyone/elisaviihde
 
 import requests, json, re, time, datetime, math
